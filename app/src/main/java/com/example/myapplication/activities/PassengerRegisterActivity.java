e13c6b3a4cf571c4d8716b20a21934c69ef8f4a6
SelenaMilutin <selena.milutin@gmail.com>
2022-11-11 16:58:11 +0100

    Add gui for passenger registration

---
+++ app/src/main/java/com/example/myapplication/activities/PassengerRegisterActivity.java
@@ -0,0 +1,33 @@
+package com.example.myapplication.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.myapplication.R;
+import com.example.myapplication.databinding.ActivityMainBinding;
+import com.google.android.material.snackbar.Snackbar;
+
+public class PassengerRegisterActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passenger_register);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        Button button = (Button) findViewById(R.id.register_summbit_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Snackbar.make(view, "Please confirm registration on your email", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+    }
+}
