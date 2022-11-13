8b71c8ec2f0b338cba842d10643a54ffb5fc8257
unknown <spremic.katarina@gmail.com>
2022-11-13 22:27:42 +0100

    Add toolbars for entire gui

--- app/src/main/java/com/example/myapplication/activities/PassengerAccountActivity.java
+++ app/src/main/java/com/example/myapplication/activities/PassengerAccountActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.View;
@@ -18,5 +19,17 @@ public class PassengerAccountActivity extends AppCompatActivity {
         setContentView(R.layout.activity_passenger_account);
         View view = findViewById(R.id.passacc_view);
         PassengerAccountAdapter adapter = new PassengerAccountAdapter(UsersMokap.getUserByEmail("examplemail@sally.com"), view);
+
+        Toolbar toolbar = findViewById(R.id.passenger_account_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Account");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/myapplication/activities/PassengerMainActivity.java
+++ app/src/main/java/com/example/myapplication/activities/PassengerMainActivity.java
@@ -1,8 +1,11 @@
 package com.example.myapplication.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -21,36 +24,39 @@ public class PassengerMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passenger_main);
-        Toolbar toolbar = findViewById(R.id.passenger_toolbar);
+
+        Toolbar toolbar = findViewById(R.id.passenger_main_toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Uber");
 
-        mTitle = getTitle();
-
-    }
-
-    @Override
-    public void setTitle(CharSequence title) {
-        mTitle = title;
-        getSupportActionBar().setTitle(mTitle);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_passenger, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
-
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_passenger_ride_history) {
-            Toast.makeText(PassengerMainActivity.this, "Clicked ride history", Toast.LENGTH_SHORT).show();
+        if (id == R.id.action_passenger_account) {
+            this.startActivity(new Intent(this, PassengerAccountActivity.class));
             return true;
         }
-
+        if (id == R.id.action_passenger_ride_history) {
+            this.startActivity(new Intent(this, PassengerRideHistoryActivity.class));
+            return true;
+        }
+        if (id == R.id.action_passenger_inbox) {
+            this.startActivity(new Intent(this, PassengerInboxActivity.class));
+            return true;
+        }
         return super.onOptionsItemSelected(item);
+
     }
+
 
 }
