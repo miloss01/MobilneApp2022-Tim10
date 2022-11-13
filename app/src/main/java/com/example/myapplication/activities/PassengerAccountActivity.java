8d4cf1fc280cb12341b63f887e5b25a8f82335c2
unknown <spremic.katarina@gmail.com>
2022-11-13 14:10:35 +0100

    Add mockup data for PassengerAccount

--- app/src/main/java/com/example/myapplication/activities/PassengerAccountActivity.java
+++ app/src/main/java/com/example/myapplication/activities/PassengerAccountActivity.java
@@ -3,8 +3,12 @@ package com.example.myapplication.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.myapplication.R;
+import com.example.myapplication.adapters.PassengerAccountAdapter;
+import com.example.myapplication.tools.UsersMokap;
 
 public class PassengerAccountActivity extends AppCompatActivity {
 
@@ -12,5 +16,7 @@ public class PassengerAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passenger_account);
+        View view = findViewById(R.id.passacc_view);
+        PassengerAccountAdapter adapter = new PassengerAccountAdapter(UsersMokap.getUserByEmail("examplemail@sally.com"), view);
     }
 }
--- app/src/main/java/com/example/myapplication/models/User.java
+++ app/src/main/java/com/example/myapplication/models/User.java
@@ -10,6 +10,12 @@ public class User {
     private boolean blockedFlag;
     private String phone;
 
+    public User(String name, String lastName, String email) {
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
     public String getName() {
         return name;
     }
