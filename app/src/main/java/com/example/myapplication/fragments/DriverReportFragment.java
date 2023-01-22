990e46d6b00f63f4f3b95e71eb432268b11609fc
SelenaMilutin <selena.milutin@gmail.com>
2023-01-22 11:38:25 +0100

    Insert charts into project structure

--- app/src/main/java/com/example/myapplication/fragments/DriverReportFragment.java
+++ app/src/main/java/com/example/myapplication/fragments/DriverReportFragment.java
@@ -1,5 +1,6 @@
 package com.example.myapplication.fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +8,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.myapplication.R;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +29,15 @@ import com.example.myapplication.R;
  * create an instance of this fragment.
  */
 public class DriverReportFragment extends Fragment {
+    private View view;
+    private EditText dateFromEditText;
+    private EditText dateToEditText;
+    private TextView totalKmTextView;
+    private TextView averageKmTextView;
+    private TextView totalRideTextView;
+    private TextView averageRideTextView;
+    private BarChart kmBarChart;
+    private BarChart rideNumBarChart;
 
 
     public DriverReportFragment() {
@@ -39,6 +61,114 @@ public class DriverReportFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_driver_report, container, false);
+        view = inflater.inflate(R.layout.fragment_driver_report, container, false);
+        dateFromEditText = view.findViewById(R.id.editTextDate);
+        dateToEditText= view.findViewById(R.id.editTextDate2);
+        totalKmTextView = view.findViewById(R.id.total_km_report);
+        averageKmTextView = view.findViewById(R.id.average_km_report);
+        totalRideTextView = view.findViewById(R.id.total_ride_report);
+        averageRideTextView = view.findViewById(R.id.average_ride_report);
+        kmBarChart = view.findViewById(R.id.barChart_km);
+        rideNumBarChart = view.findViewById(R.id.barChart_ride);
+        view.findViewById(R.id.report_driver).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initBarChart(kmBarChart);
+                initBarChart(rideNumBarChart);
+                generateReport();
+            }
+        });
+        return view;
+    }
+
+    private void generateReport() {
+        fillChartWithData(kmBarChart, "Kilometers");
+        fillChartWithData(rideNumBarChart, "Number of rides");
+    }
+
+    private void fillChartWithData(BarChart barChart, String title) {
+        ArrayList<Double> valueList = new ArrayList<Double>();
+        ArrayList<BarEntry> entries = new ArrayList<>();
+//        String title = "Title";
+
+        //input data
+        for(int i = 0; i < 6; i++){
+            valueList.add(i * 100.1);
+        }
+
+        //fit the data into a bar
+        for (int i = 0; i < valueList.size(); i++) {
+            BarEntry barEntry = new BarEntry(i, valueList.get(i).floatValue());
+            entries.add(barEntry);
+        }
+
+        BarDataSet barDataSet = new BarDataSet(entries, title);
+        initBarDataSet(barDataSet);
+
+        BarData data = new BarData(barDataSet);
+        barChart.setData(data);
+        barChart.invalidate();
+    }
+
+    private void initBarDataSet(BarDataSet barDataSet){
+        //Changing the color of the bar
+        barDataSet.setColor(Color.parseColor("#304567"));
+        //Setting the size of the form in the legend
+        barDataSet.setFormSize(15f);
+        //showing the value of the bar, default true if not set
+        barDataSet.setDrawValues(false);
+        //setting the text size of the value of the bar
+        barDataSet.setValueTextSize(12f);
+    }
+
+    private void initBarChart(BarChart barChart){
+        //hiding the grey background of the chart, default false if not set
+        barChart.setDrawGridBackground(false);
+        //remove the bar shadow, default false if not set
+        barChart.setDrawBarShadow(false);
+        //remove border of the chart, default false if not set
+        barChart.setDrawBorders(false);
+
+        //remove the description label text located at the lower right corner
+        Description description = new Description();
+        description.setEnabled(false);
+        barChart.setDescription(description);
+
+        //setting animation for y-axis, the bar will pop up from 0 to its value within the time we set
+        barChart.animateY(1000);
+        //setting animation for x-axis, the bar will pop up separately within the time we set
+        barChart.animateX(1000);
+
+        XAxis xAxis = barChart.getXAxis();
+        //change the position of x-axis to the bottom
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        //set the horizontal distance of the grid line
+        xAxis.setGranularity(1f);
+        //hiding the x-axis line, default true if not set
+        xAxis.setDrawAxisLine(false);
+        //hiding the vertical grid lines, default true if not set
+        xAxis.setDrawGridLines(false);
+
+        YAxis leftAxis = barChart.getAxisLeft();
+        //hiding the left y-axis line, default true if not set
+        leftAxis.setDrawAxisLine(false);
+
+        YAxis rightAxis = barChart.getAxisRight();
+        //hiding the right y-axis line, default true if not set
+        rightAxis.setDrawAxisLine(false);
+
+        Legend legend = barChart.getLegend();
+        //setting the shape of the legend form to line, default square shape
+        legend.setForm(Legend.LegendForm.LINE);
+        //setting the text size of the legend
+        legend.setTextSize(11f);
+        //setting the alignment of legend toward the chart
+        legend.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
+        //setting the stacking direction of legend
+        legend.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        //setting the location of legend outside the chart, default false if not set
+        legend.setDrawInside(false);
+
     }
 }
--- app/src/main/java/com/example/myapplication/fragments/DriverStatisticFragment.java
+++ app/src/main/java/com/example/myapplication/fragments/DriverStatisticFragment.java
@@ -7,11 +7,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.myapplication.R;
 
 
 public class DriverStatisticFragment extends Fragment {
+    private View view;
+    private TextView acceptanceRateTextView;
+    private TextView workingHoursTextView;
+    private TextView incomeTextView;
 
 
     public DriverStatisticFragment() {
@@ -35,6 +42,40 @@ public class DriverStatisticFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_driver_statistic, container, false);
+        view = inflater.inflate(R.layout.fragment_driver_statistic, container, false);
+        incomeTextView = view.findViewById(R.id.incoome);
+        workingHoursTextView = view.findViewById(R.id.working_h);
+        acceptanceRateTextView = view.findViewById(R.id.acceptance);
+        view.findViewById(R.id.radio_day_stat).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updatedayStatistics();
+            }
+        });
+        view.findViewById(R.id.radio_month_stat).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateMonthStatistics();
+            }
+        });
+        view.findViewById(R.id.radio_year_stat).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateYearStatistics();
+            }
+        });
+        return view;
+    }
+
+    private void updateYearStatistics() {
+        acceptanceRateTextView.setText("god");
+    }
+
+    private void updateMonthStatistics() {
+        acceptanceRateTextView.setText("god");
+    }
+
+    private void updatedayStatistics() {
+        acceptanceRateTextView.setText("god");
     }
 }
